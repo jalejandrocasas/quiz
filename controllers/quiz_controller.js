645cdcecb8c9a657e535da594cec0ee6519a31ef
Alejandro <ventosilladesanjuan@gmail.com>
2015-07-08 18:51:36 +0200

    Creada la página author

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,13 +1,13 @@
 //GET /quizes/question
 exports.question = function(req, res){
-	res.render('quizes/question' , {pregunta: 'Capital de Italia'});
+	res.render('quizes/question' , {pregunta: 'Capital de Italia', title: 'Pregunta'});
 }
 
 //GET /quizes/answer
 exports.answer = function(req, res){
 	if(req.query.respuesta === 'Roma'){
-		res.render('quizes/answer' , {respuesta: 'Correcto'});
+		res.render('quizes/answer' , {respuesta: 'Correcto', title: 'Repuesta'});
 	} else {
-		res.render('quizes/answer' , {respuesta: 'Incorrecto'});	
+		res.render('quizes/answer' , {respuesta: 'Incorrecto', title: 'Repuesta'});	
 	}
 }
--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,15 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+/*GET question page*/
 router.get('/quizes/question', quizController.question);
+
+/*GET answer page*/
 router.get('/quizes/answer', quizController.answer);
+
+/*GET author page*/
+router.get('/author', function(req, res) {
+  res.render('author', { title: 'Author' });
+});
 
 module.exports = router;
