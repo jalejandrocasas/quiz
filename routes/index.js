b9db2f885b8c91bb8c0a28603cb3fe07798fcb09
alejandro <alejandro.casas@marketingrepublic.es>
2015-08-03 07:51:52 +0200

    Crear, validar, editar y borrar preguntas

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var quizController = require('../controllers/quiz_controller');
 //Pagina de entrada
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index');
+  res.render('index', {errors:[]});
 });
 
 //Autoload de comandos con :quizId
@@ -18,6 +18,17 @@ router.get('/quizes', quizController.index)
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 /*GET answer page*/
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+/*GET new quiz form page*/
+router.get('/quizes/new', quizController.new);
+/*POST create quiz*/
+router.post('/quizes/create', quizController.create);
+/*GET edit form page*/
+router.get('/quizes/:quizId(\\d+)/edit', quizController.edit);
+/*PUT edit quiz*/
+router.put('/quizes/:quizId(\\d+)', quizController.update)
+/*DELETE quiz*/
+router.delete('/quizes/:quizId(\\d+)', quizController.destroy);
+
 
 //Página de author
 /*GET author page*/
